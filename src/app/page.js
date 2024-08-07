b575311431932b8ca4a87235e7a97897c08958a0
YAEL SAROKA <47431986@ort>
2024-08-07 10:19:52 -0300

    termiando

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,92 @@
+"use client";
+
 import Image from "next/image";
+import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [paises, setPaises] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [paisActual, setPaisActual] = useState(null);
+  const [valor, setValor] = useState("");
+  const [puntaje, setPuntaje] = useState(0);
+  const [respuesta, setRespuesta] = useState("");
+
+  useEffect(() => {
+    const puntajeGuardado = localStorage.getItem("puntaje");
+    if (puntajeGuardado) {
+      setPuntaje(parseInt(puntajeGuardado, 10));
+    }
+
+    const consultarAPI = async () => {
+      const url = "https://countriesnow.space/api/v0.1/countries/flag/images";
+      try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        if (resultado.data && resultado.data.length > 0) {
+          setPaises(resultado.data);
+          setTodos(resultado.data); // Guarda la lista original
+          seleccionarPaisAleatorio(resultado.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    consultarAPI();
+  }, []);
+
+  const seleccionarPaisAleatorio = (listaDePaises) => {
+    if (listaDePaises.length === 0) {
+      setRespuesta("¡Has adivinado todas las banderas! Reiniciando...");
+      setTimeout(() => {
+        setPaises(todos);
+        seleccionarPaisAleatorio(todos);
+      }, 2000);
+      return;
+    }
+    const paisAleatorio =
+      listaDePaises[Math.floor(Math.random() * listaDePaises.length)];
+    setPaisActual(paisAleatorio);
+  };
+
+  const handleSubmit = () => {
+    let nuevoPuntaje;
+    if (paisActual && valor.toLowerCase() === paisActual.name.toLowerCase()) {
+      nuevoPuntaje = puntaje + 10;
+      setRespuesta("¡Correcto! <3");
+    } else {
+      nuevoPuntaje = puntaje - 1;
+      setRespuesta(
+        `Incorrecto!! :P La respuesta correcta es ${paisActual.name}.`
+      );
+    }
+
+    setPuntaje(nuevoPuntaje);
+    localStorage.setItem("puntaje", nuevoPuntaje); // guarda el puntaje en localStorage
+
+    const paisesRestantes = paises.filter(
+      (pais) => pais.name !== paisActual.name
+    );
+    if (paisesRestantes.length === 0) {
+      setRespuesta("Nos quedamos sin banderas...");
+      setPaises(todos); // Restaura la lista original
+      seleccionarPaisAleatorio(todos);
+    } else {
+      setPaises(paisesRestantes);
+      seleccionarPaisAleatorio(paisesRestantes);
+    }
+
+    setValor("");
+  };
+
+  const handleChange = (e) => {
+    setValor(e.target.value);
+    localStorage.setItem("valor", e.target.value);
+  };
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
@@ -15,80 +95,34 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            By{" Yae y Cami "}
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.card}>
+        <p>Puntaje: {puntaje}</p>
+        <div className={styles.center}>
+          <h1>¡Adivina la bandera!</h1>
+        </div>
+        <div className={styles.contenido}>
+          {paisActual && (
+            <div>
+              <img src={paisActual.flag} width={200} height={100} id="imagen" />
+            </div>
+          )}
+        </div>
+        <div className={styles.contenido}>
+          <input type="text" value={valor} onChange={handleChange} />
+        </div>
+        <div className={styles.contenido}>
+          <p id="slato">{respuesta}</p>
+        </div>
+        
+        <div className={styles.contenido2}>
+          <button className={styles.button1} type="button" onClick={handleSubmit}>
+            Adivinar . . .
+          </button>
+        </div>
       </div>
     </main>
   );
